c604fdf013862e63f4a993bf6ea294d4fe92b216
Michael Stiles <mstiles01@icloud.com>
2020-08-22 14:00:20 -0500

    finished react portion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import AppHeader from './components/AppHeader';
 import AppFooter from './components/AppFooter';
+import Home from './components/Home';
 class App extends Component {
   render() {
     return <Fragment>
       <AppHeader />
+      <Home />
       <AppFooter />
     </Fragment>;
   }
